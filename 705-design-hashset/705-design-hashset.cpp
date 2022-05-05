6750c638ed1380493294a6f1dc54ff7221fa1461
Md Atif <64218887+Atif0604@users.noreply.github.com>
2022-05-05 22:36:18 +0530

    Time: 930 ms (5.00%), Space: 327.7 MB (6.01%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,23 +1,57 @@
 class MyHashSet {
 public:
+   // Approach 1: if size is upto 10^7 
+//     vector <int> n;
+//     int size;
+//     MyHashSet() {
+//         size=1e6+1;//as the index is to made of 10^6
+//         n.resize(size);
+//     }
     
-    vector <int> n;
+//     void add(int key) {
+//         n[key]=1;
+//     }
+    
+//     void remove(int key) {
+//         n[key]=0;
+//     }
+    
+//     bool contains(int key) {
+//         return n[key];
+//     }
+// };
+    
+    
+//     Approach 2 : if size >10^7
+    vector <list<int>> n;
     int size;
     MyHashSet() {
         size=1e6+1;//as the index is to made of 10^6
         n.resize(size);
     }
-    
+    int hash(int key){
+        return key%size;
+    }
+    list<int>::iterator search(int key){
+        int i=hash(key);
+        return find(n[i].begin(),n[i].end(),key);
+    }
     void add(int key) {
-        n[key]=1;
+        if (contains(key))return;
+        int i=hash(key);
+        n[i].push_back(key);
     }
     
     void remove(int key) {
-        n[key]=0;
+        if (!contains(key))return;
+        int i=hash(key);
+        n[i].erase(search(key));
     }
     
     bool contains(int key) {
-        return n[key];
+        int i=hash(key);
+        if (search(key) !=n[i].end()) return true;
+        else return false;
     }
 };
 
