8eddb3bd59ac94724e53d0aa1f6bfe55e0b65916
Md Atif <64218887+Atif0604@users.noreply.github.com>
2022-05-08 22:29:31 +0530

    Time: 812 ms (5.01%), Space: 331.5 MB (5.11%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,23 +1,63 @@
 class MyHashMap {
 public:
-    vector<int>n;
+//     Apporach 1: when we make a vector of given range
+//     vector<int>n;
+//     int size;
+//     MyHashMap() {
+//         size=1e6+1;
+//         n.resize(size);
+//         fill(n.begin(),n.end(),-1);
+//     }
+    
+//     void put(int key, int value) {
+//         n[key]=value;
+//     }
+    
+//     int get(int key) {
+//         return n[key];
+//     }
+    
+//     void remove(int key) {
+//         n[key]=-1;
+//     }
+// };
+//     Approach 2 : if 
+     vector<list<pair<int,int>>>n;
     int size;
     MyHashMap() {
         size=1e6+1;
         n.resize(size);
-        fill(n.begin(),n.end(),-1);
     }
-    
+    int hash(int key){
+        return key%size;
+    }
+    list <pair<int,int>> :: iterator search(int key){
+        int i=hash(key);
+        list <pair<int,int>> :: iterator it = n[i].begin();
+        while(it != n[i].end()){
+            if (it->first == key)
+                return it;
+                it++;
+        }
+        return it;
+    }
     void put(int key, int value) {
-        n[key]=value;
+        int i=hash(key);
+        remove(key);
+        n[i].push_back({key,value});
     }
     
     int get(int key) {
-        return n[key];
+        int i=hash(key);
+        list <pair<int,int>> :: iterator it = search(key);
+        if(it == n[i].end()) return -1;
+        else return (it->second);
     }
     
     void remove(int key) {
-        n[key]=-1;
+        int i=hash(key);
+         list <pair<int,int>> :: iterator it = search(key);
+        if(it != n[i].end()) n[i].erase(it);
     }
 };
 
