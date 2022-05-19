59f9d2ad32be53fc6657fcb9f4b5ff6c5cc72efa
Md Atif <64218887+Atif0604@users.noreply.github.com>
2022-05-19 23:56:08 +0530

    Time: 18 ms (6.05%), Space: 8.6 MB (10.82%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -9,18 +9,38 @@
  * };
  */
 
-// Approach 1: Iterative 
+// // Approach 1: Iterative 
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         if(head == NULL) return NULL;
+//         ListNode *prev=NULL,*curr=head,*next=head->next;
+//         while(curr != NULL){
+//             curr->next=prev;
+//             prev=curr;
+//             curr=next;
+//             if(next!=NULL) next=next->next;
+//         }
+//         return prev;    
+//     }
+// };
+
+// Approach 2: Recursive
 class Solution {
 public:
+    ListNode* reverse(ListNode* head) 
+    {
+          if(head->next == NULL)
+          {
+              return head;
+          }
+        ListNode *reverseHead = reverse(head->next);
+        head->next->next= head;
+        head->next=NULL;
+        return reverseHead;
+    }
     ListNode* reverseList(ListNode* head) {
-        if(head == NULL) return NULL;
-        ListNode *prev=NULL,*curr=head,*next=head->next;
-        while(curr != NULL){
-            curr->next=prev;
-            prev=curr;
-            curr=next;
-            if(next!=NULL) next=next->next;
-        }
-        return prev;    
+        if(head == NULL)return NULL;
+          return reverse(head);
     }
 };
