12a07a7b71906270ea79bb03d9d85ae9fac35028
Md Atif <64218887+Atif0604@users.noreply.github.com>
2022-05-25 23:35:40 +0530

    Time: 17 ms (17.14%), Space: 14.9 MB (44.26%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -9,24 +9,62 @@
  * };
  */
 
-// Approach 1: Recursive with using the same structure
+// // Approach 1: Recursive with using the same structure
+// class Solution {
+// public:
+    
+//     ListNode* merge(ListNode* l1, ListNode* l2){
+//         if(l1==NULL)return l2;
+//         if(l2==NULL)return l1;
+//         if(l1->val < l2->val){
+//             l1->next=merge(l1->next,l2);
+//             return l1;
+//         }
+//         else{
+//             l2->next=merge(l1,l2->next);
+//             return l2;
+//         }
+        
+//     }
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         return merge(list1,list2);
+//     }
+// };
+
+
+// Approach 2: Iterative with using the same structure
 class Solution {
 public:
-    
-    ListNode* merge(ListNode* l1, ListNode* l2){
-        if(l1==NULL)return l2;
-        if(l2==NULL)return l1;
-        if(l1->val < l2->val){
-            l1->next=merge(l1->next,l2);
-            return l1;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL)return list2;
+        if(list2==NULL)return list1;
+        ListNode *ans=NULL,*tail=NULL;
+        if(list1->val < list2->val){
+            ans=list1;
+            tail=list1;
+            list1=list1->next;
+            
         }
         else{
-            l2->next=merge(l1,l2->next);
-            return l2;
+            ans=list2;
+            tail=list2;
+            list2=list2->next;
         }
-        
-    }
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        return merge(list1,list2);
+        while(list1!=NULL && list2!=NULL){
+        if(list1->val < list2->val){
+            tail->next=list1;
+            tail=list1;
+            list1=list1->next;
+            
+        }
+        else{
+            tail->next=list2;
+            tail=list2;
+            list2=list2->next;
+        }
+       }
+        if(list1==NULL) tail->next=list2;
+        else tail->next=list1;
+        return ans;
     }
 };
