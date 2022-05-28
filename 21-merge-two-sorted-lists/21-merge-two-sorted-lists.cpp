2c3d34885409ae5211ae15012eef6d70e47dc06e
Md Atif <64218887+Atif0604@users.noreply.github.com>
2022-05-28 20:22:08 +0530

    Time: 18 ms (13.50%), Space: 14.8 MB (44.18%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -32,24 +32,53 @@
 // };
 
 
-// Approach 2: Iterative with using the same structure
+// // Approach 2: Iterative with using the same structure (ans LL)
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if(list1==NULL)return list2;
+//         if(list2==NULL)return list1;
+//         ListNode *ans=NULL,*tail=NULL;
+//         if(list1->val < list2->val){
+//             ans=list1;
+//             tail=list1;
+//             list1=list1->next;
+            
+//         }
+//         else{
+//             ans=list2;
+//             tail=list2;
+//             list2=list2->next;
+//         }
+//         while(list1!=NULL && list2!=NULL){
+//         if(list1->val < list2->val){
+//             tail->next=list1;
+//             tail=list1;
+//             list1=list1->next;
+            
+//         }
+//         else{
+//             tail->next=list2;
+//             tail=list2;
+//             list2=list2->next;
+//         }
+//        }
+//         if(list1==NULL) tail->next=list2;
+//         else tail->next=list1;
+//         return ans;
+//     }
+// };
+
+
+
+// Approach 2.1: Iterative with using the same structure(dummy node)
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if(list1==NULL)return list2;
         if(list2==NULL)return list1;
-        ListNode *ans=NULL,*tail=NULL;
-        if(list1->val < list2->val){
-            ans=list1;
-            tail=list1;
-            list1=list1->next;
-            
-        }
-        else{
-            ans=list2;
-            tail=list2;
-            list2=list2->next;
-        }
+        ListNode *ans=new ListNode(-1);//for making attachment
+        ListNode *tail=ans;
         while(list1!=NULL && list2!=NULL){
         if(list1->val < list2->val){
             tail->next=list1;
@@ -65,6 +94,6 @@ public:
        }
         if(list1==NULL) tail->next=list2;
         else tail->next=list1;
-        return ans;
+        return ans->next;
     }
 };
