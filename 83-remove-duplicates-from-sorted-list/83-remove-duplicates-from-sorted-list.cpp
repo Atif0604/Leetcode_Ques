9c2ccc3b552a7dc81a1cf0fc81bc0a1ec965893e
Md Atif <64218887+Atif0604@users.noreply.github.com>
2022-06-02 00:05:48 +0530

    Time: 8 ms (90.81%), Space: 11.6 MB (37.34%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -9,16 +9,37 @@
  * };
  */
 
-// Approach 1: Recursive Approach
+// // Approach 1: Recursive Approach
+// class Solution {
+// public:
+//     ListNode* deleteDuplicates(ListNode* head) {
+//         if(head==NULL || head->next==NULL)return head;
+//         ListNode *newHead = deleteDuplicates(head->next);
+//         if(head->val == newHead->val)return newHead;
+//         else{
+//             head->next = newHead;
+//             return head;
+//         }
+//     }
+// };
+
+// Approach 2: Iterative Approach
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(head==NULL || head->next==NULL)return head;
-        ListNode *newHead = deleteDuplicates(head->next);
-        if(head->val == newHead->val)return newHead;
-        else{
-            head->next = newHead;
-            return head;
+        ListNode *temp=head;
+        while(temp->next!=NULL){
+            if(temp->val == temp->next->val){
+                ListNode *del= temp->next;
+                temp->next=del->next;
+                delete del;
+                }
+            else{
+                temp=temp->next;
+                
+            }
         }
+        return head;
     }
 };
